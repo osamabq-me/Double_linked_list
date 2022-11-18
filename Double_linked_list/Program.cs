49a5395b222611fb09c968e28f32e74ac12977ff
OSAMAH  ALMEKHLAFI <osamah.b.ft21@mail.umy.ac.id>
2022-11-18 16:52:19 +0700

    Create Class for list variables

--- Double_linked_list/Program.cs
+++ Double_linked_list/Program.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace Double_linked_list
 {
+    class Node
+    {
+        /*Node class represent the node of doubl Linked list
+         * it consists of the informaiton part and links to
+         * its succeding and preceding
+         * in termes of next and perviuos
+         * */
+
+        public int noMhs;
+        public string name;
+        //point to the succeding node
+        public Node next;
+        //points to yhe preceeding node
+        public Node prev;
+    }
 
     class Program
     {
